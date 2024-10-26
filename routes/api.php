6f0a02d6981617517f2430a97a7b0843688afa75
themestermim <themestermim@gmail.com>
2024-10-26 16:41:02 +0330

    auth controller installed

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,14 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::group(['middleware' => ['auth:api']], function () {
 
 });
 
 Route::group(['middleware' => ['guest:api']], function () {
-
+    Route::post('/register', [RegisterController::class,'register']);
 });
 
 Route::get('/', function () {
